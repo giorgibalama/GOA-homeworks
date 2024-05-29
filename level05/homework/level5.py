3e47d56f92ea302c010fb58ce2e5e24dd756a29d
giorgibalama <george.balamtsarashvili2010@gmail.com>
2024-05-29 18:29:20 +0400

    homework

---
+++ level05/homework/level5.py
@@ -0,0 +1,20 @@
+name="giorgi"
+last_name="balamtsarashvili"
+age=13
+address="tbilisi vazisubani "
+favorite_food="chicken"
+favorite_color="red, white"
+favorite_car="porsche 918 spyder"
+
+
+
+
+
+
+
+
+
+
+
+
+
