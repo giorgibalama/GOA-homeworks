c455417020c3e3a224a50c1915a6fbe16dab9b4a
giorgibalama <george.balamtsarashvili2010@gmail.com>
2024-06-24 17:37:01 +0400

    homework

---
+++ level13/homework/lesson13.py
@@ -0,0 +1,22 @@
+# first homework 
+#1.take four numbers 
+4,6,12,34,
+#make mathmatical equations on all of them,like  +,-,*,//,**
+print(4+6)
+print(12-34)
+print(34*6)
+print(12//4)
+print(6**34)
+#second homework
+name='giorgi'
+last_name='balamwarashvili'
+age=13
+country='georgia'
+city='tbilisi'
+favorite_color='red, white'
+favorite_car='porche 918 spyder'
+favorite_food='stake'
+favorite_sport='basketball'
+print('my name is'+ name, 'i am'+ age, 'i live in'+ country, 'my favorite color is'+ favorite_color, 'my favorite car is'+ favorite_car, 'my favorite food is'+ favorite_food, 'my favorite sport is'+ favorite_sport) 
+
+
