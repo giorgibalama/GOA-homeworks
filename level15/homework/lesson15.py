875c89b63689fead22887d26e2ce244e73d997cf
giorgibalama <george.balamtsarashvili2010@gmail.com>
2024-07-04 15:32:24 +0400

    homework

---
+++ level15/homework/lesson15.py
@@ -0,0 +1,55 @@
+#დავალება1: გაკეთეთ and და or ოპერატორებზე 10 მაგალითი. 
+a=333
+b=212
+a>b
+print(True or False)
+
+n=453
+w=1112
+n>w
+print(True or False)
+
+l=56
+q=3
+l<q
+print(True or False)
+
+v=45
+m=23
+v>m
+print(True or False)
+
+a=333
+b=457
+a>b
+print(True or False)
+
+g=14
+h=12
+i=34
+o=7
+print(o>i and g<h)
+
+g=14
+h=12
+i=34
+o=7
+print(o<i and g>h)
+
+g=14
+h=12
+i=34
+o=7
+print(o>i and g<h)
+
+g=14
+h=12
+i=34
+o=7
+print(o>i and g<h)
+
+g=14
+h=12
+i=34
+o=7
+print(o>i and g<h)
