e84fcf986dcbd56fbab626055957b46b6bc3c8cb
giorgibalama <george.balamtsarashvili2010@gmail.com>
2024-07-18 14:14:38 +0400

    homework

---
+++ level19/lesson19.py
@@ -0,0 +1,10 @@
+#homework n1
+
+for loop in range(30):
+    print('GOA IS THE BEST!')
+    #homework n2
+for loop in range (5, 150):
+    print(loop)
+       #homework n3
+for loop in range (2, 54, 4):
+    print(loop) 
---
+++ level20/homework/script.js
@@ -0,0 +1,5 @@
+let username;
+document.getElementById("mysubmit").onclick = function(){
+    username = document.getElementById("mytext").value
+    document.getElementById("myh1").textContent = `hello ${username}`
+    }
