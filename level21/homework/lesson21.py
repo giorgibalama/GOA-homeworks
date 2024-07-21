0ebb832ba9ae409df174421c9426dc289b3c736d
giorgibalama <george.balamtsarashvili2010@gmail.com>
2024-07-21 18:04:34 +0400

    homework

---
+++ level21/homework/lesson21.py
@@ -0,0 +1,13 @@
+import time
+for second in range (126, 0, -1):
+    print(second)
+    time.sleep(0.100)
+print( "GOA IS THE GOAT!!!")    
+
+
+
+message="fasten you seatbelts"
+
+
+for i in range (3):
+    print(message)
