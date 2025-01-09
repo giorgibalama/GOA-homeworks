9fbe22f14e06ac52d5cd372abbc73788744199f4
giorgibalama <george.balamtsarashvili2010@gmail.com>
2025-01-09 15:29:47 +0400

    homework

---
+++ level69/homework/script.js
@@ -0,0 +1,89 @@
+
+
+
+
+//alert - ის და console.log - ის გამოყენებით, გააკეთეთ 10-10 მაგალითი, ყველა მათემათიკურ ოპერატორზე(+,-,*,/,**)
+//console.log(5+5);
+
+//console.log(5-5);
+
+//console.log(5*5);
+
+//console.log(5/5);
+
+//console.log(5**5);
+
+//console.log(10+10);
+
+//console.log(10-10);
+
+//console.log(10*10);
+
+//console.log(10/10);
+
+//console.log(10**10);
+
+//alert(5+5);
+
+//alert(5-5);
+
+//alert(5*5);
+
+//alert(5/5);
+
+//alert(5**5);
+
+//alert(10+10);
+
+//alert(10-10);
+
+//alert(10*10);
+
+//alert(10/10);
+
+//alert(10**10);
+//alert - ის და console.log - ის გამოყენებით, ორი სიტყვა შეართეთ ერთმანეთს
+
+//console.log("Hello" + "World");
+//alert("Hello" + "World");
+
+
+//alert - ის და console.log - ის გამოყენებით, შექმენით შემდეგი სტრუქტურა.
+//The world will be destroyed in 10 seconds
+//The world will be destroyed in 9 seconds
+//The world will be destroyed in 8 seconds
+//გამოიტანეთ იგივე ტექსტი 0 ის ჩათვლთ
+
+alert("The world will be destroyed in 10 seconds");
+alert("The world will be destroyed in 9 seconds");
+alert("The world will be destroyed in 8 seconds");
+alert("The world will be destroyed in 7 seconds");
+alert("The world will be destroyed in 6 seconds");
+alert("The world will be destroyed in 5 seconds");
+alert("The world will be destroyed in 4 seconds");
+alert("The world will be destroyed in 3 seconds");
+alert("The world will be destroyed in 2 seconds");
+alert("The world will be destroyed in 1 seconds");
+alert("The world will be destroyed in 0 seconds");
+
+console.log("The world will be destroyed in 10 seconds");
+console.log("The world will be destroyed in 9 seconds");
+console.log("The world will be destroyed in 8 seconds");
+console.log("The world will be destroyed in 7 seconds");
+console.log("The world will be destroyed in 6 seconds");
+console.log("The world will be destroyed in 5 seconds");
+console.log("The world will be destroyed in 4 seconds");    
+console.log("The world will be destroyed in 3 seconds");
+console.log("The world will be destroyed in 2 seconds");
+console.log("The world will be destroyed in 1 seconds");
+console.log("The world will be destroyed in 0 seconds");
+
+
+
+//როდესაც "alert"-ს იყენებ რასაც დაწერ მაგ კოდში იმას გამოიტანს ვებსაიტის ჩატვირთვის თანავე
+
+//როდესაც "console.log"-ს იყენებ რასაც დაწერ მაგ კოდში იმას გამოიტანს კონსოლში
+
+//javascript არის როგორც ვებსაიტის ტვინი რომელიც მას ფუნქციას აძლევს და ვებსაიტი ჯავასკრიპტის გარეშე არ მუშაობს
+
+
